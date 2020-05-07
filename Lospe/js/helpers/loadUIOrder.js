7a60605bf9af5523cdc5a94c4c9427c0cbca2dcb
HQD <tuongtukhoc@gmail.com>
2020-05-07 16:38:35 +0700

    Lospe

---
+++ Lospe/js/helpers/loadUIOrder.js
@@ -0,0 +1,77 @@
+import Storage from "./localStorage.js";
+
+export default class UIOrder {
+
+    loadUI(productsList) {
+        var products = productsList.products;
+
+        var $orderProducts = $(".order-products");
+        var $orderContainer = $(".order-products .order-container");
+        
+        var count = 0;
+        var limit = 4;
+        var $orderRowCardsItem = $("<div>");
+        $orderRowCardsItem.addClass("order-row-cards-item");
+        $orderContainer.append($orderRowCardsItem);
+
+        for(var i = 0; i < products.length; i++) {
+            var $orderCard = $("<div>");
+            $orderCard.addClass("order-card");
+            $orderCard.attr("data-id", products[i].id);
+
+            var $orderImage = $("<div>");
+            $orderImage.addClass("order-image");
+            var $img = $("<img>");
+            $img.addClass("img-fluid");
+            $img.attr("src", products[i].image);
+            $img.attr("alt", products[i].name);
+            $orderImage.append($img);
+
+            var $title = $("<div>");
+            $title.addClass("title");
+            $title.html(`<h5>${products[i].name}</h5>`)
+
+            var $price = $("<div>");
+            $price.addClass("price");
+            $price.html(`<p>${products[i].price} vnđ</p>`)
+            
+            var $moreInforButton = $("<div>");
+            $moreInforButton.addClass("more-infor-button");
+            var $a = $("<a>");
+            $a.html("chi tiết");
+            $a.attr("href", "./item.html?id=" + products[i].id);
+
+            $moreInforButton.append($a);
+
+            var $contentBox = $("<div>");
+            $contentBox.addClass("content-box");
+            $contentBox.append($title);
+            $contentBox.append($price);
+            $contentBox.append($moreInforButton);
+
+            $orderCard.append($orderImage);
+            $orderCard.append($contentBox);
+
+            if (count < limit) {
+                $orderRowCardsItem.append($orderCard);
+            } else {
+                count = 0;
+                $orderRowCardsItem = $("<div>");
+                $orderRowCardsItem.addClass("order-row-cards-item");
+                $orderContainer.append($orderRowCardsItem);
+                $orderRowCardsItem.append($orderCard);
+            }
+
+            count++;
+        }
+
+        var $cart = $(".header .navbar .navbar-collapse .navbar-nav .cart-situation");
+        var $amount = $(".header .navbar .navbar-collapse .navbar-nav .nav-item .amount");
+        $amount.html(`${Storage.countAmount()}`);
+        // Watch cart click
+        $cart.on("click", function() {
+            window.document.location = "./cart.html";
+        });
+
+    }
+}
